7cab972333126e57e03eb162e09f0267a2cf99c8
Stanislav <yastva3@gmail.com>
2022-11-28 16:37:04 +0600

    Introduce to jsonschema, pydantic

--- src/baseclasses/response.py
+++ src/baseclasses/response.py
@@ -1,4 +1,4 @@
-from jsonschema import validate
+# from jsonschema import validate
 
 from src.enums.global_enums import GloballErrorMessages
 
@@ -13,9 +13,10 @@ class Response:
     def validate(self, schema):
         if isinstance(self.response_json, list):
             for item in self.response_json:
-                validate(item, schema)
+                schema.parse_obj(item)
         else:
-            validate(self.response_json, schema)
+            schema.parse_obj(self.response_json)
+        return self
 
     def check_status_code(self, status_code):
         if isinstance(status_code, list):
--- tests/smth_test.py
+++ tests/smth_test.py
@@ -2,11 +2,13 @@ import requests
 
 from configurations import SERVICE_URL
 from src.baseclasses.response import Response
-from src.schemas.post import POST_SCHEMA
-
-r = requests.get(url=SERVICE_URL)
-response = Response(r)
+# from src.schemas.post import POST_SCHEMA
+from src.pydantic_schemas.post import Post
 
 
 def test_get_posts():
-    response.check_status_code(200).validate(POST_SCHEMA)
+    r = requests.get(url=SERVICE_URL)
+    response = Response(r)
+    print(r.json())
+    response.check_status_code(200).validate(Post)
+ 
