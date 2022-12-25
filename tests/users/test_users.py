f30b5fe64196cdd7b213cd2dbefa640e798997f6
Stanislav <yastva3@gmail.com>
2022-12-25 23:54:40 +0600

    Create allure report

--- tests/users/test_users.py
+++ tests/users/test_users.py
@@ -5,14 +5,23 @@ from src.pydantic_schemas.user import User, TestUser
 
 
 def test_getting_users_list(get_users):
+    '''
+    In this test we check, that http code is ok and response corresponds to schema
+    '''
     Response(get_users).assert_status_code(200).validate(User)
 
 
 def test_user_schema(get_content):
+    '''
+    In this test json scheme from a1qa is tested
+    '''
     Content(get_content).validate(TestUser)
 
 
 def test_address(get_content):
+    '''
+    In this test we check that address field is not empty
+    '''
     con = Content(get_content)
     for item in con.content:
         assert len(item["address"]) != 0, f"Error here-> id: {item['id'], 'address is: ', item['address']}"
@@ -21,6 +30,9 @@ def test_address(get_content):
 @pytest.mark.production
 @pytest.mark.skip("not needed yet")
 def test_another():
+    '''
+    In this test we check, that 1 is equal to 1
+    '''
     assert 1 == 1
 
 
@@ -33,4 +45,7 @@ def test_another():
     ('a', 'b', None),
 ])
 def test_calculator(first_val, second_val, result, calculate):
+    '''
+    In this test we are testing calculation of two values (valid and invalid)
+    '''
     assert calculate(first_val, second_val) == result
